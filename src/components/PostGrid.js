bccabe22b0a4204d6650b1bdd4b006e55df53f3d
andy94077 <andy94077@gmail.com>
2021-01-09 21:57:32 +0800

    Add RWD for PostGrid and PostPreview

--- src/components/PostGrid.js
+++ src/components/PostGrid.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { GridList } from "@material-ui/core";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import PostPreview from "./PostPreview";
+import CustomModal from "./CustomModal";
+import Post from "./Post";
 
 export default function PostGrid(props) {
-  const { posts, cellHeight = 250, cols = 3 } = props;
+  const { posts, cols = 3 } = props;
+  const theme = useTheme();
+  const widthMatches = useMediaQuery(theme.breakpoints.up("md"));
+  const [openModal, setOpenModal] = useState(false);
+  const [modalContent, setModalContent] = useState({});
+
+  const handleClickPost = (post) => () => {
+    setModalContent(post);
+    setOpenModal(true);
+  };
 
   return (
-    <GridList
-      cellHeight={cellHeight}
-      cols={cols}
-      style={{ width: cellHeight * cols }}
-    >
-      {posts.map((post) => (
-        <PostPreview post={post} />
-      ))}
-    </GridList>
+    <>
+      <GridList cellHeight="auto" cols={cols}>
+        {posts.map((post) => (
+          <PostPreview
+            key={post.id}
+            post={post}
+            onClick={handleClickPost(post)}
+            style={
+              widthMatches
+                ? {
+                    width: `${100 / cols - 1}%`,
+                    margin: "0.5%",
+                    paddingTop: `${100 / cols - 1}%`,
+                  }
+                : {
+                    width: `${100 / cols}%`,
+                    paddingTop: `${100 / cols}%`,
+                  }
+            }
+          />
+        ))}
+      </GridList>
+      <CustomModal open={openModal} setOpen={setOpenModal}>
+        <Post post={modalContent} />
+      </CustomModal>
+    </>
   );
 }

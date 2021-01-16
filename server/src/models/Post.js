9640253cc9ed05ed2dbacb763f979fa471cc5df6
b06902024 <47936298+b06902024@users.noreply.github.com>
2021-01-17 00:05:19 +0800

    init post schema

---
+++ server/src/models/Post.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const CommentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    default: "",
+  },
+  content: {
+    type: String,
+    default: "",
+  },
+});
+
+const PostSchema = new Schema(
+  {
+    userID: {
+      type: Schema.Types.ObjectId,
+      required: [true, "userID field is required."],
+      ref: "users",
+    },
+    type: {
+      type: String,
+      required: [true, "type field is required."],
+      enum: ["normal", "findModel", "findSnapper"],
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+    time: Date,
+    location: {
+      type: String,
+      default: "",
+    },
+    styles: {
+      type: [String],
+      default: [],
+    },
+    images: {
+      type: [String],
+      default: [],
+    },
+    likes: {
+      type: [String],
+      default: [],
+    },
+    comments: {
+      type: [CommentSchema],
+      default: [],
+    },
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
+
+// Creating a table within database with the defined schema
+const Post = mongoose.model("posts", PostSchema);
+
+// Exporting table for querying and mutating
+export default Post;

ab683c7e210f4b1b34aed37f4aacc56f2d56cec0
b06902024 <47936298+b06902024@users.noreply.github.com>
2021-01-17 02:04:39 +0800

    init post seed and api

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -1,7 +1,16 @@
 import { Router } from "express";
 import UserController from "../controllers/UserController";
+import PostController from "../controllers/PostController";
 
 const router = Router();
 router.route("/user").get(UserController.index).post(UserController.create);
+router
+  .route("/post")
+  .get(PostController.index)
+  .post(PostController.create)
+  .put(PostController.update)
+  .delete(PostController.delete)
+  .put(PostController.like)
+  .put(PostController.comment);
 
 export default router;
---
+++ server/src/seed/PostTableSeeder.js
@@ -0,0 +1,57 @@
+import Post from "../models/Post";
+import User from "../models/User";
+
+const PostTableSeeder = {
+  name: "posts",
+  delete: async () => Post.deleteMany({}),
+  create: async () => {
+    const users = await User.find();
+    const posts = [
+      {
+        userID: users[0]._id,
+        type: "normal",
+        content: "andy",
+        time: new Date("2021-01-17"),
+        localtion: "Taipei City",
+        styles: ["style 1", "style 2"],
+        images: ["/avatars/cat.jpg", "/avatars/dog.jpg"],
+        likes: ["user 1", "user 2"],
+        comments: [
+          { name: "chiachia", content: "hi" },
+          { name: "benson", content: "HAHAHA" },
+        ],
+      },
+      {
+        userID: users[1]._id,
+        type: "findModel",
+        content: "chiachia",
+        time: Date.now(),
+        localtion: "Taipei City",
+        styles: ["style 1", "style 2"],
+        images: ["/avatars/cat.jpg", "/avatars/dog.jpg"],
+        likes: ["user 1", "user 2"],
+        comments: [
+          { name: "andy", content: "hi" },
+          { name: "benson", content: "HAHAHA" },
+        ],
+      },
+      {
+        userID: users[0]._id,
+        type: "findModel",
+        content: "benson",
+        time: Date.now(),
+        localtion: "Taipei City",
+        styles: ["style 1", "style 2"],
+        images: ["/avatars/cat.jpg", "/avatars/dog.jpg"],
+        likes: ["user 1", "user 2"],
+        comments: [
+          { name: "andy", content: "hi" },
+          { name: "chiachia", content: "HAHAHA" },
+        ],
+      },
+    ];
+    await Post.create(posts);
+  },
+};
+
+export default PostTableSeeder;

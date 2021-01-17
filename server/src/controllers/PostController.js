7d8512a213f34ab9bf8998638ca4bbf47a80c871
b06902024 <47936298+b06902024@users.noreply.github.com>
2021-01-17 23:06:46 +0800

    fix comment

--- server/src/controllers/PostController.js
+++ server/src/controllers/PostController.js
@@ -1,21 +1,5 @@
 import Post from "../models/Post";
-
-const PostParser = (req) => {
-  const data = {};
-
-  if (req.body.postID) data.postID = req.body.postID;
-  data.userID = req.body.userID;
-  data.token = req.body.token;
-  data.type = req.body.type;
-  if (req.body.content) data.content = req.body.content;
-  if (req.body.time) data.time = new Date(req.body.time);
-  if (req.body.location) data.location = req.body.location;
-  if (req.body.styles) data.styles = req.body.styles;
-  if (req.body.images) data.images = req.body.images;
-  if (req.body.username) data.username = req.body.username;
-
-  return data;
-};
+import User from "../models/User";
 
 const PostController = {
   index: async (req, res) => {
@@ -24,7 +8,7 @@ const PostController = {
     return res.json({ posts });
   },
   create: async (req, res) => {
-    const data = PostParser(req);
+    const data = req.body;
     if (!data.userID)
       return res.status(403).json({ msg: "userID field is required" });
     if (!data.token)
@@ -44,7 +28,7 @@ const PostController = {
     }
   },
   update: async (req, res) => {
-    const data = PostParser(req);
+    const data = req.body;
     if (!data.userID) {
       return res.status(403).json({ msg: "userID field is required" });
     }
@@ -63,7 +47,7 @@ const PostController = {
     }
   },
   delete: async (req, res) => {
-    const data = PostParser(req);
+    const data = req.body;
     if (!data.userID) {
       return res.status(403).json({ msg: "userID field is required" });
     }
@@ -82,8 +66,7 @@ const PostController = {
     }
   },
   like: async (req, res) => {
-    console.log(req.body);
-    const data = PostParser(req);
+    const data = req.body;
     if (!data.userID) {
       return res.status(403).json({ msg: "userID field is required" });
     }
@@ -105,7 +88,7 @@ const PostController = {
     }
   },
   comment: async (req, res) => {
-    const data = PostParser(req);
+    const data = req.body;
     if (!data.userID) {
       return res.status(403).json({ msg: "userID field is required" });
     }
@@ -115,16 +98,14 @@ const PostController = {
     if (!data.postID) {
       return res.status(403).json({ msg: "postID field is required" });
     }
-    if (!data.username) {
-      return res.status(403).json({ msg: "comment field is required" });
-    }
     if (!data.comment) {
       return res.status(403).json({ msg: "comment field is required" });
     }
+    const user = await User.findById(data.userID);
     const filter = { _id: data.postID };
     try {
       const msg = await Post.updateOne(filter, {
-        $push: { comments: { name: data.username, content: data.comment } },
+        $push: { comments: { name: user.name, content: data.comment } },
       });
       return res.json({ msg: "Success" });
     } catch (err) {
